f0db73c30f8028880115afc3ed61961384c7a220
Jay <31196222+jaybaysay@users.noreply.github.com>
2018-06-24 08:29:35 -0700

    Update dice_rolling_simulator.py

--- dice_rolling_simulator/dice_rolling_simulator.py
+++ dice_rolling_simulator/dice_rolling_simulator.py
@@ -1,3 +1,6 @@
+"""
+Rolls a dice until the user decides to stop.
+"""
 import random
 
 min_int = 1
@@ -9,12 +12,26 @@ ask_to_roll_again = 'Do you want to roll again? Y/N'
 error_ask_to_roll_again = 'Try Again. Acceptable Answers are Y/N'
 
 def roll_dice():
+  """
+  Randomly picks an integer between 1 and 6 then prints it.
   
+  Args: None
+  
+  Returns: None
+  """
   dice_roll = random.randint(min_int,max_int)
   print("You rolled {}!" .format(dice_roll))
 
 
 def main():
+    """
+    Rolls a dice. User is given an option to roll again (Y) or not (N).
+    Y means roll again. N means the event is completed.
+    
+    Args: None
+    
+    Returns: None
+    """
     roll_dice()
 
     while True:
